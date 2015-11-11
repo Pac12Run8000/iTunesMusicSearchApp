3748040ab64c420837688d504cf4bee628d15ea7
Pac12Run8000 <norbert298@gmail.com>
2015-11-10 17:07:21 -0800

    functionality completed 0.03

---
+++ iTunesMusicSearchApp/MusicView.swift
@@ -0,0 +1,25 @@
+//
+//  MusicView.swift
+//  iTunesMusicSearchApp
+//
+//  Created by MIchelle Grover on 11/6/15.
+//  Copyright © 2015 Norbert Grover. All rights reserved.
+//
+
+import UIKit
+
+class MusicView: UIView {
+
+    @IBOutlet weak var imgArtwork: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblArtist: UILabel!
+    @IBOutlet weak var lblGenre: UILabel!
+    
+    func addDataToMusicView(album:Album) {
+        self.imgArtwork.image = UIImage(data: NSData(contentsOfURL: NSURL(string: album.artworkURL)!)!) 
+        self.lblTitle.text = album.title
+        self.lblArtist.text = album.artist
+        self.lblGenre.text = album.genre        
+    }
+
+}
--- iTunesMusicSearchApp/ViewController.swift
+++ iTunesMusicSearchApp/ViewController.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var txtSearch: UITextField!
+    @IBOutlet weak var scrollResults: UIScrollView!
+    @IBOutlet weak var pageIndicator: UIPageControl!
+    
+    var numberOfItems = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        iTunesConnection.getAlbumForString("Drake") { (album:Album) -> () in
-            print(album.title)
-        }
+        
+        self.pageIndicator.numberOfPages = 0
+        self.scrollResults.delegate = self
+        
+        
     }
 
+    @IBAction func btnSearchForMusic(sender: AnyObject) {
+        self.txtSearch.resignFirstResponder()
+        iTunesConnection.getAlbumForString(self.txtSearch.text!) { (album:Album) -> () in
+            
+            self.numberOfItems++
+            self.pageIndicator.numberOfPages = self.numberOfItems
+            
+            self.scrollResults.contentSize = CGSizeMake(CGFloat(self.numberOfItems) * self.scrollResults.frame.size.width, self.scrollResults.frame.size.height)
+            
+        let musicView = NSBundle.mainBundle().loadNibNamed("MusicView", owner: self, options: nil).last as! MusicView
+            musicView.frame = CGRectMake(CGFloat(self.numberOfItems - 1) * self.scrollResults.frame.size.width, 0, self.scrollResults.frame.size.width, self.scrollResults.frame.size.height)
+            musicView.updateConstraints()
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                musicView.addDataToMusicView(album)
+                self.scrollResults.addSubview(musicView)
+                
+                self.scrollResults.scrollRectToVisible(musicView.frame, animated: true)
+            })
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        let page = Int(self.scrollResults.contentOffset.x/self.scrollResults.frame.size.width)
+        pageIndicator.currentPage = page
+    }
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
